9cc90e9c884533e8ac6cf16bce33158211067147
Brique Au Bob <briqueaubob@gmail.com>
2022-07-15 17:50:40 +0200

    fix

--- app/Controllers/Http/StatsController.ts
+++ app/Controllers/Http/StatsController.ts
@@ -1,9 +1,9 @@
-import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 import User from "App/Models/User"
 
 export default class StatsController {
-    public async get(ctx: HttpContextContract) {
+    public async get() {
         return {
             members: await User.query().getCount()
         }
