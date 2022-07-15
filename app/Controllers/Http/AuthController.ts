e24c670513e23b273497a135191a85ce5f37c7a8
Brique Au Bob <briqueaubob@gmail.com>
2022-07-15 13:42:38 +0200

    first commit

---
+++ app/Controllers/Http/AuthController.ts
@@ -0,0 +1,73 @@
+// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+import User from "App/Models/User"
+
+export default class AuthController {
+    public async redirect ({ ally, request }) {
+        const redirect = await ally.use(request.param('provider')).redirectUrl();
+        return {
+            redirect
+        }
+    }
+
+    public async authorize({ ally, request, auth }) {
+        const provider = await ally.use(request.param('provider'))
+
+        /**
+         * User has explicitly denied the login request
+         */
+        if (provider.accessDenied()) {
+            return 'Access was denied'
+        }
+
+        /**
+         * Unable to verify the CSRF state
+         */
+        if (provider.stateMisMatch()) {
+            return 'Request expired. Retry again'
+        }
+
+        /**
+         * There was an unknown error during the redirect
+         */
+        if (provider.hasError()) {
+            return provider.getError()
+        }
+
+        /**
+         * Get the user profile from the provider
+         */
+        const user = await provider.user()
+
+        /**
+         * Find or create a user using the provider's user id
+         */
+        const dbUser = await User.firstOrCreate({
+            discord_id: user.id,
+        }, {
+            discord_id: user.id,
+            email: user.email,
+            username: user.nickName,
+            discriminator: user.original.discriminator,
+            avatar: user.avatarUrl,
+        })
+
+        /**
+         * Generate token from user
+         */
+        const token = await auth.use('api').generate(dbUser, {
+            expiresIn: '7days'
+        })
+
+        return {
+            user: dbUser,
+            token
+        }
+    }
+
+    public async auth({ auth }) {
+        return {
+            user: auth.user,
+        }
+    }
+}
---
+++ start/routes.ts
@@ -0,0 +1,29 @@
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| This file is dedicated for defining HTTP routes. A single file is enough
+| for majority of projects, however you can define routes in different
+| files and just make sure to import them inside this file. For example
+|
+| Define routes in following two files
+| ├── start/routes/cart.ts
+| ├── start/routes/customer.ts
+|
+| and then import them inside `start/routes.ts` as follows
+|
+| import './routes/cart'
+| import './routes/customer'
+|
+*/
+
+import Route from '@ioc:Adonis/Core/Route'
+
+Route.group(() => {
+  Route.get('user', 'AuthController.auth').middleware('auth').as('user')
+  Route.group(() => {
+    Route.get(':provider', 'AuthController.redirect').as('redirect')
+    Route.get(':provider/callback', 'AuthController.authorize').as('authorize')
+  }).prefix('oauth2').as('oauth2');
+}).prefix('auth').as('authentification');
