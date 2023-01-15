4f6fc78853a72a7595ec0b44d2b21feac274b3a2
BriqueAuBob <briqueaubob@gmail.com>
2023-01-15 10:46:51 +0100

    set zen admin

--- app/Middleware/IsAdmin.ts
+++ app/Middleware/IsAdmin.ts
@@ -7,10 +7,10 @@ export default class IsAdmin {
     next: () => Promise<void>
   ) {
     const admins = [
-      "307531336388968458",
-      "364008742637010975",
-      "365124713623846913",
-      "530357272531042304",
+      "307531336388968458", // bob
+      "364008742637010975", // draks
+      "530357272531042304", // willz
+      "429621241071140887", // zen
     ];
     if (!auth.user || !admins.includes(auth.user.discord_id)) {
       /**
